b19bacbcc1e9357254afecb13650b0043678288e
Anton2891 <anton08@bk.ru>
2021-12-15 15:44:16 +0300

    result dz-39

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Path path = Paths.get("file.txt");
+        String [] str = new String[]{"Hello world!!!", "Hello Java!!!"};
+            try {
+                Files.write(path, Arrays.asList(str));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        try {
+            System.out.println(Files.readAllLines(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
